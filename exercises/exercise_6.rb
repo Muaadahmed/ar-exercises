fb51f2d9a4269d19ec9f8a5bb35bc83e74367c9f
Muaadahmed <muaad.abdi.ahmed@gmail.com>
2022-01-05 23:52:58 +0000

    link store and employees table and generate multiple employees in store 1 and store 2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Kuwait", last_name: "Alaghrabi", hourly_rate: 75)
+@store1.employees.create(first_name: "Kelly", last_name: "Hunter", hourly_rate: 90)
+
+@store2.employees.create(first_name: "Keith", last_name: "Vander", hourly_rate: 45)
+@store2.employees.create(first_name: "Olympus", last_name: "O'Reilly", hourly_rate: 65)
+@store2.employees.create(first_name: "Taki", last_name: "Doritos", hourly_rate: 85)
