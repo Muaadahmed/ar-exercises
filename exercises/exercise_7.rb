b2cfee72b251f0ef534b12a716bcda8d25a42622
Muaadahmed <muaad.abdi.ahmed@gmail.com>
2022-01-06 00:49:30 +0000

    Add validation to each module and check validations

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,9 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+# @store_name = User.find_by(name: "Burnaby")
+# @new_store = Store.create(name: "Henderson")
+print "Enter Store name: "
+user_input = gets.chomp
+@user_store = Store.create(name: user_input)
+pp @user_store.errors.messages
